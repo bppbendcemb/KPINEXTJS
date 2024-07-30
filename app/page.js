e90e43e9e47647ee89d4f8b3f0ad3ff4620f0877
ben dcemb <bendcemb@gmail>
2024-07-30 09:12:46 +0700

    2024-07-30

--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10); // จำนวนรายการต่อหน้า
 
+ 
+
   // ตั้งค่า inputYear เป็นปีปัจจุบันเมื่อหน้าโหลด
   useEffect(() => {
     const currentYear = new Date().getFullYear();
@@ -45,37 +47,57 @@ export default function Home() {
   // เปลี่ยนหน้า
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const handleYearChange = (e) => {
-    setInputYear(e.target.value);
-  };
+  // const handleYearChange = (e) => {
+  //   setInputYear(e.target.value);
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setYear(inputYear);
-    setCurrentPage(1); // รีเซ็ตหน้าเป็น 1 เมื่อเปลี่ยนปี
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setYear(inputYear);
+  //   setCurrentPage(1); // รีเซ็ตหน้าเป็น 1 เมื่อเปลี่ยนปี
+  // };
+
+  const handleYearChange = (event) =>{
+    const selectedYear = event.target.value;
+    setInputYear(selectedYear);
+    setYear(selectedYear);
+    setCurrentPage(1);
   };
 
   return (
-    <div>
+    <div className='container'>
       <h1>Data from MongoDB</h1>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <label>
-          Select Year:
+          เลือกปี :
           <select value={inputYear} onChange={handleYearChange}>
             {years.map(year => (
               <option key={year} value={year}>{year}</option>
             ))}
           </select>
         </label>
-        <button type="submit">Fetch Data</button>
-      </form>
+        <button type="submit">ยืนยันการเลือก</button>
+
+      </form> */}
+
+      <div>
+        <label>
+          เลือกปี : 
+          <select value={inputYear} onChange={handleYearChange}>
+            {years.map(year =>(
+              <option key={year} value={year}>{year}</option>
+            ))}
+          </select>
+        </label>
+      </div>
+
       <table border="1" cellPadding="5" cellSpacing="0" className='data-table'>
         <thead>
           <tr>
-            <th>Unique ID</th>
-            <th>Activity ID</th>
+            {/* <th>Unique ID</th> */}
+            <th>ID</th>
             <th>Year</th>
-            <th>Past Work Year</th>
+            <th>Past Year</th>
             <th>Target</th>
             <th>m1</th>
             <th>m2</th>
@@ -95,23 +117,23 @@ export default function Home() {
           {currentItems.length > 0 ? (
             currentItems.map((item, index) => (
               <tr key={index}>
-                <td>{item.uniqueid}</td>
+                {/* <td>{item.uniqueid}</td> */}
                 <td>{item.activityid}</td>
                 <td>{item.year}</td>
-                <td>{typeof item.pastworkyear === 'number' ? item.pastworkyear.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.target === 'number' ? item.target.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m1 === 'number' ? item.m1.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m2 === 'number' ? item.m2.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m3 === 'number' ? item.m3.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m4 === 'number' ? item.m4.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m5 === 'number' ? item.m5.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m6 === 'number' ? item.m6.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m7 === 'number' ? item.m7.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m8 === 'number' ? item.m8.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m9 === 'number' ? item.m9.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m10 === 'number' ? item.m10.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m11 === 'number' ? item.m11.toFixed(2) : 'N/A'}</td>
-                <td>{typeof item.m12 === 'number' ? item.m12.toFixed(2) : 'N/A'}</td>
+                <td>{typeof item.pastworkyear === 'number' ? item.pastworkyear.toFixed(2) : ''}</td>
+                <td>{typeof item.target === 'number' ? item.target.toFixed(2) : ''}</td>
+                <td>{typeof item.m1 === 'number' ? item.m1.toFixed(2) : ''}</td>
+                <td>{typeof item.m2 === 'number' ? item.m2.toFixed(2) : ''}</td>
+                <td>{typeof item.m3 === 'number' ? item.m3.toFixed(2) : ''}</td>
+                <td>{typeof item.m4 === 'number' ? item.m4.toFixed(2) : ''}</td>
+                <td>{typeof item.m5 === 'number' ? item.m5.toFixed(2) : ''}</td>
+                <td>{typeof item.m6 === 'number' ? item.m6.toFixed(2) : ''}</td>
+                <td>{typeof item.m7 === 'number' ? item.m7.toFixed(2) : ''}</td>
+                <td>{typeof item.m8 === 'number' ? item.m8.toFixed(2) : ''}</td>
+                <td>{typeof item.m9 === 'number' ? item.m9.toFixed(2) : ''}</td>
+                <td>{typeof item.m10 === 'number' ? item.m10.toFixed(2) : ''}</td>
+                <td>{typeof item.m11 === 'number' ? item.m11.toFixed(2) : ''}</td>
+                <td>{typeof item.m12 === 'number' ? item.m12.toFixed(2) : ''}</td>
               </tr>
             ))
           ) : (
